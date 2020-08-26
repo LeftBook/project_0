9353ef298fecbf9a611353689cdb5d322b182eba
YungChingWu <apricotleft@icloud.com>
2020-08-26 14:34:38 +0800

    add : a swift tour playground

---
+++ project_0_0_aSwiftTour.playground/Contents.swift
@@ -0,0 +1,20 @@
+//// A Swift Tour
+print("\n** A Swift Tour **\n")
+
+/// print
+print("\n* print *\n")
+
+print("Hello, world!")
+// Prints "Hello, world!".
+
+print("Hello, world with change line!\n")
+// Prints "Hello, world!"with change line.
+
+print("Hello, world after change line!")
+/// Prints "Hello, world!"after change line.
+
+print("\n* print *\n")
+// print
+
+print("\n** A Swift Tour **\n")
+//// A Swift Tour
